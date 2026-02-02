5bc563437e6c1eefbb67b19e36b4bd6250e57a55
Ilyas <weendibol21300@gmail.com>
2026-02-02 14:04:10 +0500

    refactor: remove button component, service, and type definitions from Murzex Vue UI Kit

--- packages/murzex-vue/src/shared/ui/button/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as MButton } from './button'
-export type { ButtonPropsTypes, ButtonEmitsTypes } from './types'
--- packages/murzex-vue/src/shared/ui/button/types.d.ts
+++
@@ -1,7 +0,0 @@
-export interface ButtonPropsTypes {
-	label: string
-}
-
-export interface ButtonEmitsTypes {
-	click: (e: Event) => void
-}
