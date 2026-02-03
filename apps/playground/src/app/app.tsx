5978f726bd598dda908034dfcbbe61e89beb56b7
Ilyas <weendibol21300@gmail.com>
2026-02-04 03:29:19 +0500

    feat: add multiple MButton components with varying severity styles and icons for enhanced UI options

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -5,7 +5,44 @@ import { MButton } from '@murzex-vue/mvue'
 export default defineComponent(() => {
 	return () => (
 		<>
-			<MButton value='Submit'/>
+			<div class='flex flex-col gap-2'>
+				<div class='flex flex-nowrap items-center gap-2'>
+					<MButton
+						value='Submit'
+						icons={[{ position: 'left', icon: 'account' }]}
+					/>
+					<MButton
+						value='Submit'
+						severity='secondary'
+						icons={[{ position: 'left', icon: 'account' }]}
+					/>
+					<MButton
+						value='Submit'
+						severity='success'
+						icons={[{ position: 'left', icon: 'account' }]}
+					/>
+					<MButton
+						value='Submit'
+						severity='warning'
+						icons={[{ position: 'left', icon: 'account' }]}
+					/>
+					<MButton
+						value='Submit'
+						severity='danger'
+						icons={[{ position: 'left', icon: 'account' }]}
+					/>
+					<MButton
+						value='Submit'
+						severity='info'
+						icons={[{ position: 'left', icon: 'account' }]}
+					/>
+					<MButton
+						value='Submit'
+						severity='help'
+						icons={[{ position: 'left', icon: 'account' }]}
+					/>
+				</div>
+			</div>
 		</>
 	)
 })
