da90419957513d70684a3fed484c95028c2a54ca
Ilyas <weendibol21300@gmail.com>
2026-02-03 15:35:34 +0500

    feat: enhance icon component with SVG rendering and add 'account' icon definition

--- packages/murzex-vue/src/shared/ui/icon/component.ts
+++ packages/murzex-vue/src/shared/ui/icon/component.ts
@@ -2,12 +2,31 @@ import { defineComponent, h, SetupContext } from '@vue/runtime-core'
 
 import { IconPropsTypes, IconEmitsTypes } from './types'
 
+import { icons } from './icons'
+
 export default defineComponent(
 	(props: IconPropsTypes, ctx: SetupContext<IconEmitsTypes>) => {
 		return () =>
-			h('svg', {
-				...ctx.attrs,
-			})
+			h(
+				'svg',
+				{
+					...ctx.attrs,
+					class: 'm-component-icon',
+					width: '24',
+					height: '24',
+					viewBox: '0 0 24 24',
+					fill: 'none',
+					xmlns: 'http://www.w3.org/2000/svg',
+				},
+				[
+					...icons[props.icon].map((item, index) =>
+						h('path', {
+							key: index,
+							d: item,
+						}),
+					),
+				],
+			)
 	},
 	{
 		props: ['icon'],
