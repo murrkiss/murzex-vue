0d1eea5c33b66b9368a1b9c35abfab63e8c3e48e
Ilyas <weendibol21300@gmail.com>
2026-02-05 02:28:59 +0500

    feat: add multiple severity variations to MButton component for enhanced styling options

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -5,7 +5,13 @@ import { MButton } from '@murzex-vue/mvue'
 export default defineComponent(() => {
 	return () => (
 		<>
-			<MButton value='Submit' icons={[{ position: 'left', icon: 'account' }]} />
+			<MButton value='Submit' variant='outlined' />
+			<MButton value='Submit' severity='secondary' variant='outlined' />
+			<MButton value='Submit' severity='success' variant='outlined' />
+			<MButton value='Submit' severity='warning' variant='outlined' />
+			<MButton value='Submit' severity='danger' variant='outlined' />
+			<MButton value='Submit' severity='help' variant='outlined' />
+			<MButton value='Submit' severity='info' variant='outlined' />
 		</>
 	)
 })
