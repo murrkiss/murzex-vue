b494da7cbc2e7c247d49657842e48a4a9b91c0f1
Ilyas <weendibol21300@gmail.com>
2026-02-05 23:32:28 +0500

    refactor: consolidate type exports in index.ts for improved readability and organization

--- packages/murzex-vue/src/index.ts
+++ packages/murzex-vue/src/index.ts
@@ -1,23 +1,13 @@
 import './app/assets/styles/tw.main.style.css'
 
 export { default as MIcon } from './shared/ui/icon/component'
-export type {
-	IconPropsTypes,
-	IconEmitsTypes,
-	IconType,
-} from './shared/ui/icon/types'
+export type { IconPropsTypes, IconEmitsTypes, IconType } from './shared/ui/icon/types'
 
 export { default as $MLabel } from './shared/ui/$label/component'
-export type {
-	LabelPropsTypes,
-	LabelEmitsTypes,
-	PositionType,
-} from './shared/ui/$label/types'
+export type { LabelPropsTypes, LabelEmitsTypes, PositionType } from './shared/ui/$label/types'
 
 export { default as MButton } from './shared/ui/button/component'
-export type {
-	ButtonPropsTypes,
-	ButtonEmitsTypes,
-	ButtonSeverityType,
-	ButtonSizeType,
-} from './shared/ui/button/types'
+export type { ButtonPropsTypes, ButtonEmitsTypes, ButtonSeverityType, ButtonSizeType } from './shared/ui/button/types'
+
+export { default as MButtonGroup } from './shared/ui/button-group/component'
+export type { ButtonGroupPropsTypes, ButtonGroupEmitsTypes } from './shared/ui/button-group/types'
