f33bd9846842d2aeefe7897c219144b605361417
Ilyas <weendibol21300@gmail.com>
2026-02-05 23:48:18 +0500

    refactor: optimize default slot handling in ButtonGroup component for improved readability and performance

--- packages/murzex-vue/src/shared/ui/button-group/component.ts
+++ packages/murzex-vue/src/shared/ui/button-group/component.ts
@@ -6,18 +6,16 @@ import type { ButtonGroupPropsTypes, ButtonGroupEmitsTypes } from './types'
 
 export default defineComponent(
 	(props: ButtonGroupPropsTypes, ctx: SetupContext<ButtonGroupEmitsTypes>) => {
-		let defaultSlot: VNodeChild = []
-
-		if (ctx.slots['default']) {
-			defaultSlot = ctx.slots['default']()
-		}
-
 		provide('ButtonGroupProps', {
 			size: computed(() => props.size),
 			rounded: computed(() => props.rounded),
 		})
 
-		return () => h('div', { ...ctx.attrs, class: 'm-component-button-group' }, defaultSlot)
+		return () => {
+			const defaultChildren = ctx.slots['default']
+
+			return h('div', { ...ctx.attrs, class: 'm-component-button-group' }, defaultChildren ? defaultChildren() : [])
+		}
 	},
 	{
 		props: ['size', 'rounded'],
