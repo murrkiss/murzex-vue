422f8a09d0723a4234a866e08ed50ef9ee78a8eb
Ilyas <weendibol21300@gmail.com>
2026-02-05 23:31:52 +0500

    chore: update Prettier configuration to enhance code formatting with new print width and spacing options

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import("prettier").Config} */
 
 export default {
-	trailingComma: 'all',
+	printWidth: 150,
 	tabWidth: 2,
 	useTabs: true,
+
 	semi: false,
 	singleQuote: true,
 	jsxSingleQuote: true,
+	trailingComma: 'all',
+	bracketSpacing: true,
+	bracketSameLine: false,
 	arrowParens: 'avoid',
+
 	importOrderSeparation: true,
 	importOrderSortSpecifiers: true,
 	importOrderCaseInsensitive: true,
