86a013e914ddaeb71435b85eaac363a252e27aa4
Ilyas <weendibol21300@gmail.com>
2026-02-08 04:13:30 +0500

    refactor: update MButtonSelect component to include icons for button options and comment out unused components for cleaner code

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -5,6 +5,10 @@ import { MButtonSelect, MButtonGroup, MButton, MInput } from '@murzex-vue/mvue'
 export default defineComponent(() => {
 	return () => (
 		<>
+			{/* <MButton value='Submit' size='small' icons={[{ icon: 'account', position: 'left' }]} /> */}
+			{/* <MButton value='Submit' /> */}
+			{/* <MButton value='Submit' size='large' /> */}
+
 			{/* <MButtonGroup>
 				<MButton value='Submit' />
 				<MButton value='Submit' severity='secondary' />
@@ -15,19 +19,39 @@ export default defineComponent(() => {
 				<MButton value='Submit' severity='info' />
 			</MButtonGroup> */}
 
-			{/* <MInput placeholder='Enter name:' size='small' invalid />
-			<MInput placeholder='Enter name:' />
-			<MInput placeholder='Enter name:' size='large' /> */}
+			{/* <MInput placeholder='Enter name:' size='small' invalid /> */}
+			{/* <MInput placeholder='Enter name:' /> */}
+			{/* <MInput placeholder='Enter name:' size='large' /> */}
 
+			{/* <MButtonSelect
+				buttons={[
+					{ key: '1', value: 'Ingeneer developerment', icons: [{ icon: 'account', position: 'left' }] },
+					{ key: '2', value: 'Backend developer', icons: [{ icon: 'account', position: 'left' }] },
+					{ key: '4', value: 'Frontend developer', icons: [{ icon: 'account', position: 'left' }], disabled: true },
+					{ key: '5', value: 'Fullstack developer', icons: [{ icon: 'account', position: 'left' }] },
+					{ key: '6', value: 'Devops', icons: [{ icon: 'account', position: 'left' }] },
+				]}
+				size='small'
+			/> */}
 			<MButtonSelect
 				buttons={[
-					{ key: '1', value: 'Ingeneer developerment' },
-					{ key: '2', value: 'Backend developer' },
-					{ key: '4', value: 'Frontend developer' },
-					{ key: '5', value: 'Fullstack developer' },
-					{ key: '6', value: 'Devops' },
+					{ key: '1', value: 'Ingeneer developerment', icons: [{ icon: 'account', position: 'left' }] },
+					{ key: '2', value: 'Backend developer', icons: [{ icon: 'account', position: 'left' }] },
+					{ key: '4', value: 'Frontend developer', icons: [{ icon: 'account', position: 'left' }], disabled: true },
+					{ key: '5', value: 'Fullstack developer', icons: [{ icon: 'account', position: 'left' }] },
+					{ key: '6', value: 'Devops', icons: [{ icon: 'account', position: 'left' }] },
 				]}
 			/>
+			{/* <MButtonSelect
+				buttons={[
+					{ key: '1', value: 'Ingeneer developerment', icons: [{ icon: 'account', position: 'left' }] },
+					{ key: '2', value: 'Backend developer', icons: [{ icon: 'account', position: 'left' }] },
+					{ key: '4', value: 'Frontend developer', icons: [{ icon: 'account', position: 'left' }], disabled: true },
+					{ key: '5', value: 'Fullstack developer', icons: [{ icon: 'account', position: 'left' }] },
+					{ key: '6', value: 'Devops', icons: [{ icon: 'account', position: 'left' }] },
+				]}
+				size='large'
+			/> */}
 		</>
 	)
 })
