8798f4a2c745b07cc3ceb73402e5ba4b7704882c
iengindev <weendibol21300@gmail.com>
2026-02-20 12:14:58 +0500

    chore: remove unused configuration files and workspace settings
    - Deleted pnpm-workspace.yaml to clean up workspace configuration.
    - Removed prettier.config.mjs as it is no longer needed.
    - Eliminated tsconfig.base.json to streamline TypeScript configuration.

--- packages/murzex-vue/src/shared/ui/button-group/types.d.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonSizeType } from '../button/types'
-
-export interface ButtonGroupPropsTypes {
-	size?: ButtonSizeType
-	rounded?: boolean
-}
-
-export interface ButtonGroupEmitsTypes {}
--- packages/murzex-vue/src/shared/ui/icon/types.d.ts
+++
@@ -1,9 +0,0 @@
-import { icons } from './icons'
-
-export type IconType = keyof typeof icons
-
-export interface IconPropsTypes {
-	icon: IconType
-}
-
-export interface IconEmitsTypes {}
